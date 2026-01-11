348fd391b1a30ef5de823edf288b70a6414d8b0c
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-01-11 17:50:06 -0600

    routes in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/recipe" element={<RecipeDetails />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/register" element={<Registeriation />}/>
+          <Route path='/newrecipe' element={<NewRecipe />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
