7710fb77cb77388770332c5031a948c51204c9ae
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-01-16 15:38:39 -0600

    01-16-26

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import RecipeDetails from './pages/RecipeDetails';
+import Registeriation from './pages/Registeriation';
+import NewRecipe from './pages/NewRecipe';
 function App() {
   return (
     <div className="App">
