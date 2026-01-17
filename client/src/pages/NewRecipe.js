374167f18249bd376be9464a0bfa3970afebcc60
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-01-17 10:02:12 -0600

    save

---
+++ client/src/pages/NewRecipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewRecipe =()=>{
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default NewRecipe;
---
+++ client/src/pages/Registeriation.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Registeriation=()=>{
+
+
+    return(
+        <div>
+            <label>First Name </label>
+            <input></input>
+            <br></br>
+            <label>Last Name </label>
+            <input></input>
+            <br></br>
+            <label>Email </label>
+            <input></input>
+            <br></br>
+            <label>Username </label>
+            <input></input>
+            <br></br>
+            <label>Password </label>
+            <input></input>
+        </div>
+    )
+};
+
+export default Registeriation;
