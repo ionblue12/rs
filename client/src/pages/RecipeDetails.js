8e7d44c4f413b60ca9cbc3fb252921aae41a9e02
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-03-30 15:47:21 -0500

    ingredinets good

--- client/src/pages/RecipeDetails.js
+++ client/src/pages/RecipeDetails.js
@@ -1,21 +1,23 @@
-import React, {useState, useParams, useEffect} from "react";
-
+import React, {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
 const RecipeDetails =() =>{
     const [message, setMessage] = useState('');
     const { id } = useParams();
-    console.log("id", id);
+    const [ingredients, setIngredients] = useState([]);
+    
 
     const getIngredients = async () => {
         const response = await fetch(`http://localhost:3002/api/recipes/ingredients/${id}`, {
             credentials: 'include',
         });
         const jsonResponse = await response.json();
-        console.log("ingredeinets",jsonResponse);
+        console.log("ingredeinets",jsonResponse.data);
         if(!response.ok){
             setMessage(jsonResponse.error || 'Failed to fetch ingredients');
             return;
         }
         setMessage(jsonResponse.message);
+        setIngredients(jsonResponse.data);
     };
     useEffect(() => {
         getIngredients();
@@ -23,6 +25,14 @@ const RecipeDetails =() =>{
 
     return (
         <div>
+            <h1>ing</h1>
+            {ingredients.length === 0 && <p>No Ingredients</p>}
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li key={ingredient.position}>{ingredient.ingredient_name}</li>
+                ))}
+            </ol>
+            
 
         </div>
     )
