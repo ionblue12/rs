e71260e937fd13bcd971b4bb3d796585f12561a7
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-03-30 09:16:23 -0500

    steps

--- client/src/pages/RecipeDetails.js
+++ client/src/pages/RecipeDetails.js
@@ -1,7 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, useParams, useEffect} from "react";
 
 const RecipeDetails =() =>{
     const [message, setMessage] = useState('');
+    const { id } = useParams();
+    console.log("id", id);
+
+    const getIngredients = async () => {
+        const response = await fetch(`http://localhost:3002/api/recipes/ingredients/${id}`, {
+            credentials: 'include',
+        });
+        const jsonResponse = await response.json();
+        console.log("ingredeinets",jsonResponse);
+        if(!response.ok){
+            setMessage(jsonResponse.error || 'Failed to fetch ingredients');
+            return;
+        }
+        setMessage(jsonResponse.message);
+    };
+    useEffect(() => {
+        getIngredients();
+    }, [id]);
 
     return (
         <div>
