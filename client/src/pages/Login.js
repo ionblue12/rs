e9ebef15349211b50da0059a220d8af167aabc81
ionblue12 <57737804+ionblue12@users.noreply.github.com>
2026-04-29 14:56:53 -0500

    password and login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,54 +4,62 @@ import { supabase } from '../supabaseClient';
 
 
 const Login = ()=>{
-    const [email, setEmail] = useState("");
+    const [userInfo, setuserInfo] = useState({
+            email: '',
+            password: '',
+           
+        });
     const [message, setMessage] = useState('');
-    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     
-    const handleChange = (e)=>{
-        setEmail(e.target.value);
+    const handleChange = (e)=> {
+        const { name, value } = e.target;
+        setuserInfo((prev) => ({...prev, [name]: value}));
     };
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
-        try{
-            if(loading) return;
-            setLoading(true);
-            const { error } = await supabase.auth.signInWithOtp({
-                email,
-                options: {
-                    shouldCreateUser: true,
-                    emailRedirectTo: 'http://localhost:3000/allrecipes'
-                },
-            })
-            
-            if(error){
-                setMessage(error.message || 'Login failed');
-                return;
-            }
-            setMessage(" Check your email for the login link");
-            
-            
-        }catch(error){
-            setMessage('Username or password wrong')
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: userInfo.email,
+            password: userInfo.password,
+        });
+
+        if(error){
+            setMessage(error.message || 'Login Failed');
+            return;
         }
+        setMessage("Login successful!");
+        setTimeout(() => {
+            
+            navigate('/allrecipes');
+        }, 3000);
 
     }
 
     
     return(
         <form onSubmit={handleSubmit}>
-            <h1>Please Login to see your Recipes</h1>
+            <h1>Please Login</h1>
             <label>Email </label>
             <input
+            name="email"
             type='email'
-            value={email}
+            value={userInfo.email}
             onChange={handleChange}
             required
             ></input>
             <br></br>
-            <button disabled={loading}>{loading? "Sending..." : "Login"}</button>
+            <input
+            name="password"
+            type='password'
+            value={userInfo.password}
+            onChange={handleChange}
+            required
+            ></input>
+            <br></br>
+            <button>Login</button>
+            {message && <p style={{ color: "red" }}>{message}</p>}
         </form>
     )
 }
--- client/src/pages/Registeriation.js
+++ client/src/pages/Registeriation.js
@@ -3,13 +3,12 @@ import { data, useNavigate } from "react-router-dom";
 import { supabase } from '../supabaseClient';
 
 
+
 const Registeriation=()=>{
 
     const [userInfo, setuserInfo] = useState({
-        firstname: '',
-        lastname: '',
         email: '',
-        username: '',
+        password: '',
        
     });
 
@@ -23,82 +22,32 @@ const Registeriation=()=>{
 
     const onSubmit = async (e)=>{
         e.preventDefault();
-        try{
-            const response = await fetch('http://localhost:3002/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                firstname: userInfo.firstname,
-                lastname: userInfo.lastname,
-                email: userInfo.email,
-                username: userInfo.username,
-                password: userInfo.password,
-            })
-        });
-        
-        const jsonResponse = await response.json();
 
-        if(!response.ok){
-            setMessage(jsonResponse.error || 'Registeriation failed');
+        const { data, error } = await supabase.auth.signUp({
+            email: userInfo.email,
+            password: userInfo.password,
+        });
+
+        if(error){
+            setMessage(error.message || 'Registration failed');
             return;
         }
-        setMessage(jsonResponse.message);
-        setTimeout(()=>{
-            navigate('/allrecipes', {
-            user: jsonResponse.user
-        });
-        
+        setMessage("Registration successful!");
+        setTimeout(() => {
+            navigate('/allrecipes');
         }, 3000);
-       
+        
+    };
 
-        setuserInfo({
-        firstname: '',
-        lastname: '',
-        email: '',
-        username: '',
-        password: '',
-    });
-
-    
-    }catch(error){
-        setMessage('Error adding user. Please try again');
-    }
-    }; 
     return(
         <form onSubmit={onSubmit}>
             <h2>Add New User</h2>
             {message && <div>{message}</div>}
-            <label>First Name </label>
-            <input 
-            name='firstname'
-            type="text"
-            value={userInfo.firstname}
-            onChange={handleChange}
-            ></input>
-            <br></br>
-            <label>Last Name </label>
-            <input
-            name='lastname'
-            type="text"
-            value={userInfo.lastname}
-            onChange={handleChange}
-            ></input>
-            <br></br>
             <label>Email </label>
             <input
             name='email'
             type="email"
             value={userInfo.email}
-            onChange={handleChange}
-            ></input>
-            <br></br>
-            <label>Username </label>
-            <input
-            name='username'
-            type="text"
-            value={userInfo.username}
             onChange={handleChange}
             ></input>
             <br></br>
@@ -111,6 +60,7 @@ const Registeriation=()=>{
             ></input>
             <br></br>
             <button>Submit</button>
+            <p>{message}</p>
         </form>
     )
 };
